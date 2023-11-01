ac1cf12ad005b86c13cec6b52995266510de11b7
Alex Sousa <alexcsousa2011@hotmail.com>
2023-11-01 12:27:17 -0300

    Inclusão de navegação com passagem de parametros 2

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -7,10 +7,36 @@ import {
 export default function Contacts({navigation}){
     return(
         <View>
-         <Text>João</Text>
-          <Text
-           onPress={() => navigation.navigate('Information')}
-          >Information...</Text>
+            <View style={{marginTop:30}}>
+                <Text>Nome: Alex Sousa</Text>
+                <Text>Telefone: (11) 97491-5314</Text> 
+                <Text
+                    onPress={() => navigation.navigate('Information',
+                    {
+                        nome:'Alex Carlos',
+                        telefone: '(11) 97491-5314',
+                        endereco: 'Rua dos bobos',
+                        numero:'0',
+                        profissao:'Carpinteiro',
+                        email:'alexsousa@carpintaria.com.br'
+                    })}
+                    >Clique para mais informações</Text>
+            </View>
+            <View style={{marginTop:30}}>
+                <Text>Nome: Henry Sousa</Text>
+                <Text>Telefone: (11) 97292-5314</Text> 
+                <Text
+                    onPress={() => navigation.navigate('Information',
+                    {
+                        nome:'Henry Sousa',
+                        telefone: '(11) 97292-5314',
+                        endereco: 'Rua Jose Lira Abreu',
+                        numero:'361',
+                        profissao:'Engenheiro de Software',
+                        email:'henrysousa@toptop.com.br'
+                    })}
+                    >Clique para mais informações</Text>
+            </View>         
         </View>
     )
 }
--- src/pages/Information/index.js
+++ src/pages/Information/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { View, Text } from 'react-native'
 
 
-export default function Information(){
+export default function Information({route}){
     return (
-        <View>
-            <Text>
-                Information
-            </Text>
+        <View style={{margintop:30}}>
+            <Text>Nome: {route.params.nome}</Text>
+            <Text>Telefone: {route.params.telefone}</Text>
+            <Text>Endereço: {route.params.endereco}, nº {route.params.numero}</Text>
+            <Text>Profissão: {route.params.profissao}</Text>
+            <Text>E-mail: {route.params.email}</Text>
         </View>
 
     )
