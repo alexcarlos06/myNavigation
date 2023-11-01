724b43385627d573a01162e5f5165cabd648abbd
Alex Sousa <alexcsousa2011@hotmail.com>
2023-11-01 12:26:50 -0300

    Inclusão de navegação com passagem de parametros

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,14 +1,16 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import {
+    View,
+    Text
+} from 'react-native'
 
-
-export default function Contacts(){
-    return (
+export default function Contacts({navigation}){
+    return(
         <View>
-            <Text>
-                Contacts
-            </Text>
+         <Text>João</Text>
+          <Text
+           onPress={() => navigation.navigate('Information')}
+          >Information...</Text>
         </View>
-
     )
 }
